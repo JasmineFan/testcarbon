f08228ce5da6ebb872f53efa3337274537fbb5c7
Jasmine <fancheny@cn.ibm.com>
2022-09-16 15:11:47 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './app.scss';
+import { Content, Theme } from '@carbon/react';
+import HeaderSection from './components/HeaderSection';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Theme theme="g100">
+          <HeaderSection />
+        </Theme>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
+      </>
+    );
+  }
+}
+
+export default App;
